1468d2816be91d2e49a1b0958a27fdb45861602d
lorenzorizzolo <lolloriz36@gmail.com>
2024-03-21 17:45:25 +0100

    fill the initial map

--- Rizzolo/fill.js
+++ Rizzolo/fill.js
@@ -7,19 +7,16 @@ const initial = new Map([
 
 function fill(n, map){
     let numbers = [];
-    // console.log(map)
     for (let i of map) {
         numbers.push(i.flat())
-        // console.log(i.flat())
     }
     numbers = numbers.flat()
-    // console.log(numbers)
     for(let i=0 ; i<n ; i++){
         if(numbers.indexOf(i)==-1){
             map.set(i, [])
         }
     }
-    console.log(map)
+    return map;
 }
 
 fill(10, initial);
