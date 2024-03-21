6f3ba72618fd2177b85c3dfc11ab9a3dcbdbdb62
MarcoCellini <marco.cellini@istitutoagnelli.it>
2024-03-21 17:59:08 +0100

    cellini: add data structur

--- Cellini/file_reader.js
+++ Cellini/file_reader.js
@@ -2,8 +2,9 @@ import { read_file } from "../fs_helper.js";
 
 const gestisci = (f) => {
     f = f.split('\n');
-    for (let i = 0; i < f[0]; i++) {
-        f = f.slice(1);
+    let T = f[0];
+    for (let i = 0; i < T; i++) {
+        i === 0 ? f = f.slice(1) : null;
         let [N, M] = f[0].split(' ');
         f = f.slice(1);
         let list = f.slice(0, M);
@@ -12,13 +13,14 @@ const gestisci = (f) => {
             let [x, y] = e.split(' ');
             map.has(y) ? map.set(y, [...map.get(y), x]) : map.set(y, [x]);
         });
-        return [N, M, map];            // invio alla funzione risolutrice
+        f = f.slice(M);
+        console.log(N, M, map);            // invio alla funzione risolutrice
     }
 };
 
 async function main() {
     let file = await read_file("file.txt");
-    console.log(gestisci(file));
+    gestisci(file);
 }
 
 main();
