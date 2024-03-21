9b960907f6f15e77a68ce83d0d75bee0e9dfa052
MarcoCellini <marco.cellini@istitutoagnelli.it>
2024-03-21 18:09:04 +0100

    cellini: add filler of rizzolo

--- Cellini/file_reader.js
+++ Cellini/file_reader.js
@@ -1,5 +1,19 @@
 import { read_file } from "../fs_helper.js";
 
+function fill(n, map){
+    let numbers = [];
+    for (let i of map) {
+        numbers.push(i.flat())
+    }
+    numbers = numbers.flat()
+    for(let i=0 ; i<n ; i++){
+        if(numbers.indexOf(i)==-1){
+            map.set(i, [])
+        }
+    }
+    return map;
+}
+
 const gestisci = (f) => {
     f = f.split('\n');
     let T = f[0];
@@ -11,9 +25,11 @@ const gestisci = (f) => {
         let map = new Map();
         list.map(e => {
             let [x, y] = e.split(' ');
+            x = +x; y = +y;
             map.has(y) ? map.set(y, [...map.get(y), x]) : map.set(y, [x]);
         });
         f = f.slice(M);
+        map = fill(N, map);
         console.log(N, M, map);            // invio alla funzione risolutrice
     }
 };
